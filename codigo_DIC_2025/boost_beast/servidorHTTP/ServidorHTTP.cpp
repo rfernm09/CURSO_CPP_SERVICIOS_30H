026ac23012acaf00bcb9f61eca1c7739ca2962df
aldebarran22 <antonioespinherranz@hotmail.com>
2025-12-02 10:12:19 +0100

    87

---
+++ codigo_DIC_2025/boost_beast/servidorHTTP/ServidorHTTP.cpp
@@ -0,0 +1,11 @@
+#include "ServidorHTTP.h"
+
+ServidorHTTP::ServidorHTTP(net::io_context& ioc, unsigned int port):ioc(ioc), port(port {
+
+}
+void ServidorHTTP::run() {
+
+}
+ServidorHTTP::~ServidorHTTP() {
+
+}
