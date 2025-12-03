74a5b62a4db0f6452ff9d8912fcb1a60ed9518ab
aldebarran22 <antonioespinherranz@hotmail.com>
2025-12-03 12:20:45 +0100

    87

--- codigo_DIC_2025/crow_json/servicio_empleados/EmpleadoCROW.cpp
+++ codigo_DIC_2025/crow_json/servicio_empleados/EmpleadoCROW.cpp
@@ -22,6 +22,7 @@ void EmpleadoCROW::run() {
 				return crow::response(404, "Empleado con id: " + std::to_string(id) + " no existe");
 			}
 
+			// Aplica automaticamente la funcion: to_json (de objeto a json)
 			json j = *empleado;
 			return crow::response(j.dump());
 
@@ -30,6 +31,19 @@ void EmpleadoCROW::run() {
 			return crow::response(500, std::string("Error en el servicio: ") + e.what());
 		}
 
+		});
+
+	CROW_ROUTE(app, "/empleados").methods(crow::HTTPMethod::GET)([this]() {
+
+		try {
+			auto empleados = this->repositorio.listarTodos();
+			json j = empleados;
+			return crow::response(j.dump());
+
+		}
+		catch (const std::exception& e) {
+			return crow::response(500, std::string("Error en el servicio: ") + e.what());
+		}
 		});
 
 	app.multithreaded().concurrency(std::thread::hardware_concurrency()).port(8000).run();
