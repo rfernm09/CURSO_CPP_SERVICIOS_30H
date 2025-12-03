e19770af88884b6381eeb02c1b8ace3b58b7b25b
aldebarran22 <antonioespinherranz@hotmail.com>
2025-12-03 10:25:15 +0100

    87

--- codigo_DIC_2025/bases_de_datos/mysql/EmpleadoRepositorio.cpp
+++ codigo_DIC_2025/bases_de_datos/mysql/EmpleadoRepositorio.cpp
@@ -20,22 +20,40 @@ std::optional<Empleado> EmpleadoRepositorio::read(int id)
 	return emp;
 }
 
-bool EmpleadoRepositorio::create(const Empleado&)
+bool EmpleadoRepositorio::create(const Empleado& emp)
 {
-	return false;
+	sql << "insert into empleados(id, nombre, cargo) values(:id, :nombre, :cargo)",
+		soci::use(emp.id), soci::use(emp.nombre), soci::use(emp.cargo);
+	//std::size_t filas = sql.get_affected_rows();
+	return true;
 }
 
-bool EmpleadoRepositorio::update(const Empleado&)
+bool EmpleadoRepositorio::update(const Empleado& emp)
 {
-	return false;
+	sql << "update empleados set nombre=:nombre, cargo=:cargo where id=:id",
+		soci::use(emp.nombre), soci::use(emp.cargo), soci::use(emp.id);
+	return true;
 }
 
-bool EmpleadoRepositorio::delete_(int)
+bool EmpleadoRepositorio::delete_(int id)
 {
-	return false;
+	sql << "delete from empleados where id=:id", soci::use(id);
+	return true;
 }
 
 std::vector<Empleado> EmpleadoRepositorio::select()
 {
-	return std::vector<Empleado>();
+	std::vector<Empleado> empleados;
+
+	soci::rowset<soci::row> rs = sql.prepare << "select id, nombre, cargo from empleados";
+
+	for (const auto& r : rs) {
+		Empleado emp;
+		emp.id = r.get<int>(0);
+		emp.nombre = r.get<std::string>(1);
+		emp.cargo = r.get<std::string>(2);
+		empleados.push_back(emp);
+	}
+
+	return empleados;
 }
