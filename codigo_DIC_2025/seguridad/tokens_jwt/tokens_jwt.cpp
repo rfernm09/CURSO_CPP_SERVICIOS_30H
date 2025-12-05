03fbe29ffbd987f023a01882657575fe7ea3ab65
aldebarran22 <antonioespinherranz@hotmail.com>
2025-12-05 14:04:59 +0100

    87

--- codigo_DIC_2025/seguridad/tokens_jwt/tokens_jwt.cpp
+++ codigo_DIC_2025/seguridad/tokens_jwt/tokens_jwt.cpp
@@ -2,18 +2,36 @@
 //
 
 #include <iostream>
+#include <chrono>
+#include <crow.h>
+
+#include <nlohmann/json.hpp>
+#include <jwt-cpp/jwt.h>
+
+using json = nlohmann::json;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    crow::SimpleApp app;
+        
+    CROW_ROUTE(app, "/login").methods("POST"_method)([](const crow::request& request) {
+
+        try {
+
+            // Obtener las credenciales de la request:
+            auto j = json::parse(request.body);
+
+
+        }
+        catch (const std::exception& e) {
+            return crow::response(400, std::string("Json incorrecto: ") + e.what());
+        }
+
+        });
+
+    // Poner en marcha el servidor:
+    //app.multithreaded().run();
+    app.multithreaded().concurrency(std::thread::hardware_concurrency()).port(8000).run();
+    return 0;
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
