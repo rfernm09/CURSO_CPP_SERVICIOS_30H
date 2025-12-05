863797b4785ab3e5b018d23378b544af450b418e
aldebarran22 <antonioespinherranz@hotmail.com>
2025-12-05 12:44:06 +0100

    12

---
+++ codigo_DIC_2025/hilos_punteros/punteros/Vector.cpp
@@ -0,0 +1,39 @@
+#include "Vector.h"
+
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+
+#include "Vector.h"
+
+Vector::Vector(int n)
+{
+	this->n = n;
+	this->ocupacion = 0;
+	this->numeros = new int[n];
+}
+
+bool Vector::add(int num)
+{
+	if (ocupacion + 1 < n) {
+		numeros[ocupacion] = num;
+		ocupacion++;
+		return true;
+
+	}
+	return false;
+}
+
+void Vector::imprimir()
+{
+	for (int i = 0; i < ocupacion; i++) {
+		std::cout << "i = " << i << " " << numeros[i] << std::endl;
+	}
+}
+
+Vector::~Vector()
+{
+	delete[] numeros;
+	numeros = nullptr;
+}
+
---
+++ codigo_DIC_2025/hilos_punteros/punteros/Vector.h
@@ -0,0 +1,14 @@
+#pragma once
+class Vector
+{
+private:
+	int* numeros;
+	int n;
+	int ocupacion; // indica el nro. de elementos ocupados
+
+public:
+	Vector(int = 10);
+	bool add(int);
+	void imprimir();
+	~Vector();
+}
