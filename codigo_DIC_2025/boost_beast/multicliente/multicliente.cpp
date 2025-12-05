8add94b4f8c0a467a33d45927925a7f55040d5a9
aldebarran22 <antonioespinherranz@hotmail.com>
2025-12-05 12:01:59 +0100

    87

--- codigo_DIC_2025/boost_beast/multicliente/multicliente.cpp
+++ codigo_DIC_2025/boost_beast/multicliente/multicliente.cpp
@@ -5,6 +5,7 @@
 #include <thread>
 #include <vector>
 #include <string>
+#include <memory>
 
 #include <boost/beast/core.hpp>
 #include <boost/asio.hpp>
@@ -14,7 +15,36 @@
 namespace beast = boost::beast;
 namespace websocket = beast::websocket;
 namespace net = boost::asio;
+namespace http = beast::http;
 using tcp = net::ip::tcp;
+
+class session : public std::enable_shared_from_this<session> {
+
+	// Atributos de la session:
+	tcp::socket socket_;
+	beast::flat_buffer buffer;
+	http::request<http::string_body> request;
+	http::response<http::string_body> response;
+
+public:
+	session(tcp::socket socket):socket_(std::move(socket)){}
+	void start() {}
+};
+
+
+void do_accept(tcp::acceptor& acceptor) {
+	
+	// Aceptar al cliente de forma asincrona:
+	acceptor.async_accept([&acceptor](beast::error_code ec, tcp::socket socket) {
+
+		// Si no hay error, creamos una nueva sesion:
+		if (!ec) {
+
+			// Crear y arrancar la sesion:
+			std::make_shared<session>(std::move(socket))->start();
+		}
+	});
+}
 
 
 int main()
@@ -24,7 +54,7 @@ int main()
 	// Inicializar acceptor con el contexto de entrada/salida y un endpoint en el puerto 80
 	tcp::acceptor acceptor(ioc, tcp::endpoint(tcp::v4(), 8080));
 
-	//do_accept(acceptor);
+	do_accept(acceptor);
 
 	std::cout << "Servidor multicliente en el puerto 8080" << std::endl;
 
