924879f690694179962da81cc1e6ea0507b6905d
aldebarran22 <antonioespinherranz@hotmail.com>
2025-12-05 09:28:23 +0100

    0

--- codigo_DIC_2025/boost_beast/ws_servidor/ws_servidor.cpp
+++ codigo_DIC_2025/boost_beast/ws_servidor/ws_servidor.cpp
@@ -2,18 +2,79 @@
 //
 
 #include <iostream>
+#include <thread>
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/asio.hpp>
+
+namespace beast = boost::beast;
+namespace websocket = beast::websocket;
+namespace net = boost::asio;
+using tcp = net::ip::tcp;
+
+void do_session(tcp::socket socket) {
+	try {
+
+		// DEfinir un stream con un socket TCP:
+		websocket::stream<tcp::socket> ws(std::move(socket));
+		ws.accept();
+
+		for (;;) {
+			// DEfinir un buffer para leer y escribir:;
+			beast::flat_buffer buffer;
+
+			ws.read(buffer);
+			ws.text(ws.got_text());
+
+			// Devolvemos lo mismo que hemos leido
+			ws.write(buffer.data());
+		}
+
+	}
+	catch (const beast::system_error& se) {
+		if (se.code() != websocket::error::closed) {
+			std::cerr << "Error en session: " << se.what() << std::endl;
+		}
+		else {
+			std::cout << "Cliente desconectado" << std::endl;
+		}
+	}
+	catch (const std::exception& e) {
+		std::cerr << "Error inesperado " << e.what() << std::endl;
+	}
+
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	net::io_context ioc;
+
+	// Inicializar acceptor con el contexto de entrada/salida y un endpoint en el puerto 80
+	tcp::acceptor acceptor(ioc, tcp::endpoint(tcp::v4(), 80));
+
+	// Capturar el numero de hilos que soportar el procesador:
+	unsigned int num_threads = std::thread::hardware_concurrency();
+
+	// Inicializar un pool de hilos con el numero que disponemos:
+	boost::asio::thread_pool pool(num_threads);
+
+	std::cout << "Servidor ok, numero de hilos: " << num_threads << std::endl;
+
+	for (;;) {
+
+		// DEfinir un 1 socket por conexion:
+		tcp::socket socket(ioc);
+
+		std::cout << "Esperando clientes " << std::endl;
+		
+		// Aceptar un cliente:
+		acceptor.accept(socket);
+
+		// Lanzar un hilo con boost para atender al cliente conectado:
+		boost::asio::post(pool, [s = std::move(socket)]() mutable {
+			do_session(std::move(s))
+		});
+	}
+   
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
