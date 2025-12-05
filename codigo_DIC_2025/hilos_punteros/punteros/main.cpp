8e123f83b1cb9fbb47395894540c63e6bb996657
aldebarran22 <antonioespinherranz@hotmail.com>
2025-12-05 12:50:44 +0100

    12

--- codigo_DIC_2025/hilos_punteros/punteros/main.cpp
+++ codigo_DIC_2025/hilos_punteros/punteros/main.cpp
@@ -3,7 +3,26 @@
 
 #include <iostream>
 
+#include "Vector.h"
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    Vector v1;
+
+    v1.add(1);
+    v1.add(2);
+    v1.add(3);
+    v1.add(41);
+
+    v1.imprimir();
+
+    Vector v2(v1);
+    v2.imprimir();
+
+    Vector v3;
+    v3 = v1;
+    v3.imprimir();
+
+
+    return 0;
 }
