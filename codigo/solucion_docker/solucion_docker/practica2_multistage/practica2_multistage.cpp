32f7f2680c05668091830cdb060da3e39d0e389d
aldebarran22 <antonioespinherranz@hotmail.com>
2026-03-16 13:31:43 +0100

    87

---
+++ codigo/solucion_docker/solucion_docker/practica2_multistage/practica2_multistage.cpp
@@ -0,0 +1,12 @@
+﻿// practica2_multistage.cpp: define el punto de entrada de la aplicación.
+//
+
+#include "practica2_multistage.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake desde la plantilla" << endl;
+	return 0;
+}
