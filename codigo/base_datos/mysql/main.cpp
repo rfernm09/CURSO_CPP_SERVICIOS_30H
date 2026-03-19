f5a04b55634259cc7f4d8e0e86004965bb9f52fd
aldebarran22 <antonioespinherranz@hotmail.com>
2026-03-19 10:06:48 +0100

    87

--- codigo/base_datos/mysql/main.cpp
+++ codigo/base_datos/mysql/main.cpp
@@ -3,17 +3,23 @@
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#include <mysqlx/xdevapi.h>
+
+
+void testConexion() {
+    try {
+        mysqlx::Session sesion("localhost", 33060, "root", "antonio");
+        std::cout << "Conexion ok" << std::endl;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "ERROR: " << e.what() << std::endl;
+    }
+
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
+int main()
+{
+    testConexion();
+    return 0;
+}
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
---
+++ codigo/base_datos/postgresql/postgresql.cpp
@@ -0,0 +1,30 @@
+// postgresql.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <libpq-fe.h>
+
+int testConexion() {
+    const char* conninfo = "host=127.0.0.1 port=5433 dbname=empresa3 user=antonio password=antonio";
+
+    PGconn* conn = PQconnectdb(conninfo);
+
+    if (PQstatus(conn) != CONNECTION_OK) {
+        std::cerr << "Error de conexión: " << PQerrorMessage(conn) << std::endl;
+        PQfinish(conn);
+        return 1;
+    }
+
+    std::cout << "Conexion exitosa a PostgreSQL" << std::endl;
+
+    PQfinish(conn);
+}
+
+
+
+int main()
+{
+    testConexion();
+    return 0;
+}
+
