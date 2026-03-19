f7bc7138cf076e6bfe7e99850a2241a31b04a20d
aldebarran22 <antonioespinherranz@hotmail.com>
2026-03-19 12:57:10 +0100

    87

--- codigo/crow/crow_base_datos/CategoriaCrow.cpp
+++ codigo/crow/crow_base_datos/CategoriaCrow.cpp
@@ -1,3 +1,4 @@
+#include <crow.h>
 #include "CategoriaCrow.h"
 
 CategoriaCrow::CategoriaCrow(const CategoriaRepositorio& repo):repo(repo)
@@ -6,7 +7,15 @@ CategoriaCrow::CategoriaCrow(const CategoriaRepositorio& repo):repo(repo)
 
 void CategoriaCrow::run()
 {
+	crow::SimpleApp app;
 
+	CROW_ROUTE(app, "/categorias/<int>").methods(crow::HTTPMethod::GET)([this](int id) {});
+
+	CROW_ROUTE(app, "/categorias").methods(crow::HTTPMethod::GET)([this]() {});
+
+	CROW_ROUTE(app, "/categorias/<int>").methods(crow::HTTPMethod::PUT)([this](const crow::request& req, int id) {});
+
+	CROW_ROUTE(app, "/categorias/<int>").methods(crow::HTTPMethod::Delete)([this](int id) {});
 }
 
 CategoriaCrow::~CategoriaCrow()
