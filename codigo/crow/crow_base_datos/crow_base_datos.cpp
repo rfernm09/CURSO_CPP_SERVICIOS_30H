c19d937c01322cfed785d8421d04c223e6d03bd1
aldebarran22 <antonioespinherranz@hotmail.com>
2026-03-19 12:32:56 +0100

    87

---
+++ codigo/crow/crow_base_datos/crow_base_datos.cpp
@@ -0,0 +1,10 @@
+// crow_base_datos.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
